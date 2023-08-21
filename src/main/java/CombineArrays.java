89a61f900fee0a972cd65628f0304f7f6e390507
Loza Asgedom <loza.mesfin777@gmail.com>
2023-08-21 02:20:42 +0000

    Coding Challenge: Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -9,14 +9,14 @@ public class CombineArrays {
      */
     public int[] combine(int[] arr1, int[] arr2){
 
-        //int CombinedArr = arr1.length + arr2.length;
         int[] arr3 = new int [arr1.length + arr2.length];
 
-        for (int i = 0; i<arr1.length & i<arr2.length; i++){
+        for (int i = 0; i<arr1.length; i++){
             arr3[i] = arr1[i];
-            arr3[i+arr1.length] = arr2[i];
         }
-        
+        for (int i = 0; i<arr2.length; i++){
+            arr3[i + arr1.length] = arr2[i];
+        }
         return arr3;
     }
 }
